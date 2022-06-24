253a9678af792d9582dbc779a6daffeb806716a6
neomaster <kakha81@gmail.com>
2022-06-24 22:20:29 +0400

    ADD FIRSTPAGE_01

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, View, Text, Image, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+const image = require("../mosakrebeli_reg/assets/logo_municipaluri.png");
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        backgroundColor: "white",
+      }}
+    >
+      <Text style={styles.text_header}>მუნიციპალური ინსპექცია</Text>
+      <Image source={image} style={styles.image} />
+      <Button
+        style={styles.button_1}
+        title="რეგისტრაცია"
+        color="dodgerblue"
+        onPress={() => navigation.navigate("Details")}
+      />
+      <Button
+        style={styles.button_1}
+        title="რეგისტრაცია"
+        marginTop="30"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
 }
 
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="მთავარი გვერდი" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text_header: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginTop: 100,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    marginTop: 100,
   },
 });
